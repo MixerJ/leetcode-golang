e4dc65ae37d3e59b208fda9d80a9a6a5b5b614f0
zhusw <zhusw@codoon.com>
2019-09-04 22:52:40 +0800

    完成3题

--- longestSubstringWithoutRepeatingCharacters/code/without_repeating.go
+++ longestSubstringWithoutRepeatingCharacters/code/without_repeating.go
@@ -1,54 +1,29 @@
 package code
 
 func lengthOfLongestSubstring(s string) int {
-	var returnLength int
-	var sLength = len(s)
-	if sLength == 0 {
-		return 0
-	}
-	var substringLength = 1
-	for i := 0; i < sLength; i++ {
-		var preString = string(s[i])
-		for j := i + 1; j < sLength; j++ {
-			jString := string(s[j])
-			if preString == jString {
-				if j == sLength-1 {
-					sLength = j
-					i = j - 1
-				}
-				if substringLength > sLength {
-					substringLength = sLength
-				}
-				returnLength = substringLength
-				substringLength = 1
-				break
-			} else {
-				substringLength++
+	var lens, head int
+	for k := range s {
+		t := indexs(s[head:k], s[k])
+		if t == -1 {
+			if lens < k-head+1 {
+				lens = k - head + 1
 			}
+		} else {
+			head = head + t + 1
 		}
 	}
-	if substringLength > returnLength {
-		returnLength = substringLength
-	}
-	if substringLength > sLength {
-		returnLength = sLength
-	}
-	return returnLength
+	return lens
 }
 
-//func lengthOfLongestSubstringTwo(s string) int {
-//	var returnLength int
-//	var sLength = len(s)
-//	var substringLength = 1
-//	for i := 0; i < sLength; i++ {
-//		var preString = string(s[i])
-//		for j := i + 1; j < sLength; j++ {
-//
-//		}
-//	}
-//	return returnLength
-//}
+func indexs(s string, b byte) int {
+	for k := range s {
+		if s[k] == b {
+			return k
+		}
+	}
+	return -1
+}
+
 func LengthOfLongestSubstring(s string) int {
 	return lengthOfLongestSubstring(s)
-	//return lengthOfLongestSubstringTwo(s)
 }
--- longestSubstringWithoutRepeatingCharacters/main.go
+++ longestSubstringWithoutRepeatingCharacters/main.go
@@ -6,14 +6,14 @@ import (
 )
 
 func main() {
-	//fmt.Println(code.LengthOfLongestSubstring("abcabcbb") == 3)
-	//fmt.Println(code.LengthOfLongestSubstring("bbbbb") == 1)
-	//fmt.Println(code.LengthOfLongestSubstring("pwwkew") == 3)
-	//fmt.Println(code.LengthOfLongestSubstring("au") == 2)
-	//fmt.Println(code.LengthOfLongestSubstring("b") == 1)
-	//fmt.Println(code.LengthOfLongestSubstring("aab") == 2)
-	//fmt.Println(code.LengthOfLongestSubstring("") == 0)
-	//fmt.Println(code.LengthOfLongestSubstring("bwf") == 3)
-	//fmt.Println(code.LengthOfLongestSubstring("cdd") == 2)
+	fmt.Println(code.LengthOfLongestSubstring("abcabcbb") == 3)
+	fmt.Println(code.LengthOfLongestSubstring("bbbbb") == 1)
+	fmt.Println(code.LengthOfLongestSubstring("pwwkew") == 3)
+	fmt.Println(code.LengthOfLongestSubstring("au") == 2)
+	fmt.Println(code.LengthOfLongestSubstring("b") == 1)
+	fmt.Println(code.LengthOfLongestSubstring("aab") == 2)
+	fmt.Println(code.LengthOfLongestSubstring("") == 0)
+	fmt.Println(code.LengthOfLongestSubstring("bwf") == 3)
+	fmt.Println(code.LengthOfLongestSubstring("cdd") == 2)
 	fmt.Println(code.LengthOfLongestSubstring("abba") == 2)
 }
